8e4258b21dd515d8e0d3634e9e2f3f87176f23a6
sami-l-a <samuellami696@gmail.com>
2025-03-10 00:20:26 -0700

    i add exceptions to code

--- database.php
+++ database.php
@@ -1,10 +1,23 @@
-<?php 
+<?php
 $hostname = "localhost";
 $dbusers = "root";
 $dbpassword = "";
 $dbName = "login_register";
-$conn = mysqli_connect($hostname,$dbusers,$dbpassword,$dbName);
-if(!$conn){
-    die("something went wrong:");
+
+try {
+    // Enable exception handling for mysqli
+    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
+    // Attempt to connect to the database
+    $conn = new mysqli($hostname, $dbusers, $dbpassword, $dbName);
+
+    // If the connection is successful, you can proceed with your database operations
+    echo "Connected successfully!";
+
+} catch (mysqli_sql_exception $e) {
+    // Handle the exception
+    die("Connection failed: " . $e->getMessage());
 }
+
+// You can now use the $conn object to interact with the database
 ?>
